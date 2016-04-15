ead88236660887927e3c9f305301e0ac4e752aed
Hari Prasath Raman <hraman@cs.stonybrook.edu>
2016-04-15 02:50:23 -0400

    Adding filedownloader code

---
+++ fs
@@ -0,0 +1,8 @@
+/files/one.txt	3000
+/files/two.txt	3000
+/files/three.txt	3001
+/files/four.txt	3001
+/files/five.txt	3002
+/files/six.txt	3002
+/files/seven.txt	3003
+/files/eight.txt	3003
---
+++ src/github.com/yob/graval/graval-mem/graval-mem.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/yob/graval"
+)
+
+const (
+	fileOne = "This is the first file available for download.\n\nBy Jàmes"
+	fileTwo = "This is file number two.\n\n2012-12-04"
+)
+
+type MemDriver struct {
+	fIndex map[string]string
+}
+
+func (driver *MemDriver) LoadFIndex() (err error) {
+	fData, err := ioutil.ReadFile("fs")
+	if err != nil {
+		return err
+	}
+	driver.fIndex = make(map[string]string)
+
+	files := strings.Split(string(fData), "\n")
+	for index, _ := range files {
+		parts := strings.Split(files[index], "\t")
+		parts[1] = strings.TrimSuffix(parts[1], "\n")
+		driver.fIndex[parts[0]] = parts[1]
+	}
+	return nil
+}
+func (driver *MemDriver) Authenticate(user string, pass string) bool {
+	return user == "test" && pass == "1234"
+}
+func (driver *MemDriver) Bytes(path string) (bytes int) {
+	switch path {
+	case "/one.txt":
+		bytes = len(fileOne)
+		break
+	case "/files/two.txt":
+		bytes = len(fileTwo)
+		break
+	default:
+		bytes = -1
+	}
+	return
+}
+func (driver *MemDriver) ModifiedTime(path string) (time.Time, error) {
+	return time.Now(), nil
+}
+func (driver *MemDriver) ChangeDir(path string) bool {
+	return path == "/" || path == "/files"
+}
+func (driver *MemDriver) DirContents(path string) (files []os.FileInfo) {
+	files = []os.FileInfo{}
+	switch path {
+	case "/":
+		files = append(files, graval.NewDirItem("files"))
+		files = append(files, graval.NewFileItem("one.txt", len(fileOne)))
+	case "/files":
+		files = append(files, graval.NewFileItem("two.txt", len(fileOne)))
+	}
+	return files
+}
+
+func (driver *MemDriver) DeleteDir(path string) bool {
+	return false
+}
+func (driver *MemDriver) DeleteFile(path string) bool {
+	return false
+}
+func (driver *MemDriver) Rename(fromPath string, toPath string) bool {
+	return false
+}
+func (driver *MemDriver) MakeDir(path string) bool {
+	return false
+}
+func (driver *MemDriver) GetFile(path string) (data string, err error) {
+	fmt.Println("getfile", path)
+	fData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(fData), nil
+}
+
+func (driver *MemDriver) LocateFile(path string) (port string) {
+	log.Println(path)
+	log.Printf("%+v", driver.fIndex)
+	port, exist := driver.fIndex[path]
+	if !exist {
+		port = "-1"
+	}
+	return
+}
+
+func (driver *MemDriver) PutFile(destPath string, data io.Reader) bool {
+	return false
+}
+
+type MemDriverFactory struct{}
+
+func (factory *MemDriverFactory) NewDriver() (graval.FTPDriver, error) {
+	memDriver := &MemDriver{}
+	err := memDriver.LoadFIndex()
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return memDriver, nil
+}
+
+func main() {
+	factory := &MemDriverFactory{}
+	if port, err := strconv.Atoi(os.Args[1]); err == nil {
+		ftpServer := graval.NewFTPServer(&graval.FTPServerOpts{Factory: factory, Port: port})
+		err := ftpServer.ListenAndServe()
+		if err != nil {
+			log.Print(err)
+			log.Fatal("Error starting server!")
+		}
+	}
+}
